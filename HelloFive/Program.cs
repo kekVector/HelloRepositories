a594c733d22ef78312406bf32b28466d1bb030fc
KekVector <k.o.email@yandex.ru>
2021-10-25 16:01:12 +0300

    add HelloFive HELLO Маххеевъ project

---
+++ HelloFive/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string UserName = Console.ReadLine();
+
+if (UserName.ToLower() == "маша")
+{ 
+    Console.WriteLine("ЗДАРОВА, МАХЕЕВЪ!!!");
+
+
+} 
+else
+{
+    Console.WriteLine("Тебе тут не рады, " + UserName);
+}
