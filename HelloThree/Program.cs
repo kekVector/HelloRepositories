80240c37ccbe7aa1b25e5ba14f08c1ffc61a71cd
KekVector <k.o.email@yandex.ru>
2021-10-25 16:03:12 +0300

    add Hello3 DONT WORK

---
+++ HelloThree/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число:");
+int Number1 = Console.Read();
+Console.WriteLine("Введите второе число:");
+int Number2 = Console.Read();
+Console.WriteLine(Number2);
+Console.WriteLine("Введите третье число:");
+int Number3 = Console.Read();
+Console.WriteLine(Number3);
+Console.WriteLine("Результат сложения трёх чисел");
+Console.Write(Number1 + Number2 + Number3);
