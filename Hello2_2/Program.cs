9ecdc2a62e8d50eef17f6ef8403c030e498840c6
KekVector <k.o.email@yandex.ru>
2021-11-03 15:16:59 +0300

    Add programs second lection number2

---
+++ Hello2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Ищем введенный индекс элемента в массиве
+int[] array = {1,6,3,21,54,5,43,11,9};
+int Lengh = array.Length;
+int found = int.Parse(Console.ReadLine());
+for(int i =0; i<Lengh; i++)
+{
+    if(array[i] == found)
+    {
+        Console.WriteLine($"Индекс вашего числа {i}");
+        break;
+    }
+    if(i == Lengh-1)
+    {
+        Console.WriteLine("Вашего числа в массиве нет");
+    }
+}
