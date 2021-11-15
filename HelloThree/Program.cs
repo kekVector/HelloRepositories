05ec8bba9c3ab4e450bacdc04c28054a5438b734
KekVector <k.o.email@yandex.ru>
2021-11-15 23:45:39 +0300

    работает . сложение трех

--- HelloThree/Program.cs
+++ HelloThree/Program.cs
@@ -1,10 +1,11 @@
 ﻿Console.WriteLine("Введите первое число:");
-int Number1 = Console.Read();
+int Number1 =  int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
-int Number2 = Console.Read();
+int Number2 =  int.Parse(Console.ReadLine());
 Console.WriteLine(Number2);
 Console.WriteLine("Введите третье число:");
-int Number3 = Console.Read();
+int Number3 =  int.Parse(Console.ReadLine());
 Console.WriteLine(Number3);
 Console.WriteLine("Результат сложения трёх чисел");
 Console.Write(Number1 + Number2 + Number3);
+
